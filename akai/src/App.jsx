85f560f82d3699e7af760895c3c80d372ec791f8
Ludovil <ludovic_saule@hotmail.fr>
2024-02-05 15:56:09 +0100

    tailwind config

--- akai/src/App.jsx
+++ akai/src/App.jsx
@@ -1,9 +1,12 @@
+import hero from "./assets/img/hero-akai.png";
+
 function App() {
   return (
     <div>
-      <section className="bg-green-900 h-screen">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-      </section>
+      <section
+        className="h-screen bg-cover bg-center"
+        style={{ backgroundImage: `url(${hero})` }}
+      ></section>
     </div>
   );
 }
