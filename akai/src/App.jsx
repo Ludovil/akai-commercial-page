0acc780d0336a0fa5aac9cb0d1294a85222c9d05
Ludovil <ludovic_saule@hotmail.fr>
2024-02-05 12:52:12 +0100

    boiler plate + tailwind css

---
+++ akai/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <section className="bg-green-900 h-screen">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+      </section>
+    </div>
+  );
+}
+
+export default App;
